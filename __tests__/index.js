caf262a90aef14940470982f6b60622c98d143a0
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-07-24 01:37:12 +0200

    First feature complete version with 100% tests coverage

---
+++ __tests__/index.js
@@ -0,0 +1,90 @@
+import {file} from 'tempy'
+
+import FsSet from "..";
+
+
+test('exports', function()
+{
+  expect(FsSet).toMatchInlineSnapshot(`[Function]`);
+});
+
+test('no arguments', function()
+{
+  function func()
+  {
+    new FsSet
+  }
+
+  expect(func).toThrowErrorMatchingInlineSnapshot(
+    `"The \\"path\\" argument must be of type string or an instance of Buffer or URL. Received undefined"`
+  )
+});
+
+test('arguments', function()
+{
+  const options =
+  {
+    filterAfterRead(){
+      return true
+    }
+  }
+
+  const set = new FsSet(file(), options)
+
+  expect(set).toMatchInlineSnapshot(`FsSet {}`);
+  expect(set).toHaveLength(0);
+});
+
+test('lifecycle', function()
+{
+  const set = new FsSet(file())
+
+  let result
+
+  expect(set).toHaveLength(0);
+
+  set.add('foo')
+  expect(set).toHaveLength(1);
+
+  set.add('bar')
+  expect(set).toHaveLength(2);
+
+  result = set.delete('foo')
+  expect(result).toBe(true);
+  expect(set).toHaveLength(1);
+
+  const hasFoo = set.has('foo')
+  expect(hasFoo).toBe(false);
+
+  const hasBar = set.has('bar')
+  expect(hasBar).toBe(true);
+
+  set.clear()
+  expect(set).toHaveLength(0);
+
+  result = set.delete('foo')
+  expect(result).toBe(false);
+
+  set.close()
+  set.close() // Idempotent
+
+  result = set.lock()
+
+  function func()
+  {
+    set.length
+  }
+
+  expect(func).toThrowErrorMatchingInlineSnapshot(`"closed"`)
+
+  return expect(result).rejects.toThrowErrorMatchingInlineSnapshot(`"closed"`)
+});
+
+test('lock', function()
+{
+  const set = new FsSet(file())
+
+  const promise = set.lock(function(){})
+
+  return expect(promise).resolves.toMatchInlineSnapshot(`undefined`);
+});
---
+++ index.js
@@ -0,0 +1,180 @@
+import {
+  closeSync, ftruncateSync, openSync, readSync, writeSync
+} from 'fs'
+import {EOL} from 'os'
+
+import {lock, lockSync} from 'proper-lockfile'
+
+
+const DEFAULT_BUFFER_SIZE = 16384  // 16KB, Node.js default
+
+
+function filterEmpty(line)
+{
+  return line?.length
+}
+
+
+export default class FsSet
+{
+  constructor(
+    filePath,
+    {
+      bufferSize = DEFAULT_BUFFER_SIZE,
+      filterAfterRead,
+      lockfile
+    } = {bufferSize: DEFAULT_BUFFER_SIZE}
+  ){
+    try {
+      this.#fd = openSync(filePath, 'r+')
+    } catch (error) {
+      if(error.code !== 'ENOENT') throw error
+
+      this.#fd = openSync(filePath, 'w+')
+    }
+
+    this.#buffer          = Buffer.alloc(bufferSize)
+    this.#filePath        = filePath
+    this.#filterAfterRead = filterAfterRead
+    this.#lockfileOptions = lockfile
+  }
+
+
+  //
+  // Set
+  //
+
+  get length()
+  {
+    return this.#lock(this.#read).length
+  }
+
+
+  add(value)
+  {
+    return this.#lock(this.#add, value)
+  }
+
+  clear()
+  {
+    return this.#lock(this.#write)
+  }
+
+  delete(value)
+  {
+    return this.#lock(this.#delete, value)
+  }
+
+  has(value)
+  {
+    return this.#lock(this.#read).includes(value)
+  }
+
+
+  //
+  // Public API
+  //
+
+  close()
+  {
+    if(this.#closed) return
+
+    this.#closed = true
+
+    closeSync(this.#fd)
+  }
+
+  async lock(func, ...rest)
+  {
+    if(this.#closed) throw new SyntaxError('closed')
+
+    const release = !this.#locks
+      && await lock(this.#filePath, this.#lockfileOptions)
+
+    this.#locks++
+
+    try {
+      return await func(...rest)
+    } finally {
+      this.#locks--
+
+      release?.()
+    }
+  }
+
+
+  //
+  // Private API
+  //
+
+  #buffer
+  #closed
+  #fd
+  #filterAfterRead
+  #filePath
+  #lockfileOptions
+  #locks = 0
+
+  #add = value =>
+  {
+    const values = this.#read()
+
+    values.push(value)
+
+    this.#write(values)
+  }
+
+  #delete = value =>
+  {
+    const values = this.#read()
+
+    const index = values.indexOf(value)
+    if(index < 0) return false
+
+    values.splice(index, 1)
+
+    this.#write(values)
+
+    return true
+  }
+
+  #lock(func, ...rest)
+  {
+    if(this.#closed) throw new SyntaxError('closed')
+
+    const release = !this.#locks && lockSync(this.#filePath)
+
+    try {
+      return func(...rest)
+    } finally {
+      release?.()
+    }
+  }
+
+  /**
+   * Read values file and remove non-existing processes from PIDs file
+   *
+   * @memberof FsSet
+   */
+  #read = () =>
+  {
+    const buffer = this.#buffer
+
+    const length = readSync(this.#fd, buffer, 0, buffer.length, 0)
+
+    let result = buffer.toString('utf8', 0, length)
+    .split(EOL)
+    .filter(filterEmpty)
+
+    if(this.#filterAfterRead)
+      result = result.filter(this.#filterAfterRead)
+
+    return result
+  }
+
+  #write = data =>
+  {
+    ftruncateSync(this.#fd)
+    writeSync(this.#fd, data?.join(EOL) || '', 0)
+  }
+}
