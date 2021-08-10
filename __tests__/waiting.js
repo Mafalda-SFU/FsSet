504ed212d3a56b5b79a6242982eecb91250996ca
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-08-10 21:18:28 +0200

    Fix abstract functions when class implementation is asynchronous

--- __tests__/waiting.js
+++ __tests__/waiting.js
@@ -25,3 +25,48 @@ test('closed', function()
 
   return expect(promise).rejects.toMatchInlineSnapshot(`[SyntaxError: closed]`);
 });
+
+test('lifecycle', async function()
+{
+  const set = new FsSetWaiting(file())
+
+  let result
+
+  expect(await set.length).toBe(0);
+
+  await set.add('foo')
+  expect(await set.length).toBe(1);
+
+  await set.add('bar')
+  expect(await set.length).toBe(2);
+
+  result = await set.delete('foo')
+  expect(result).toBe(true);
+  expect(await set.length).toBe(1);
+
+  const hasFoo = await set.has('foo')
+  expect(hasFoo).toBe(false);
+
+  const hasBar = await set.has('bar')
+  expect(hasBar).toBe(true);
+
+  await set.clear()
+  expect(await set.length).toBe(0);
+
+  result = await set.delete('foo')
+  expect(result).toBe(false);
+
+  set.close()
+  set.close() // Idempotent
+
+  try
+  {
+    result = await set.lock()
+  }
+  catch(error)
+  {
+    if(error.message !== 'closed') throw error
+  }
+
+  expect(result).toBe(false)
+})
